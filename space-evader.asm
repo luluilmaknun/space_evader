b708c3b206c240ce62fac503d71a96bd397180c9
giovanism <giovanmail@gmail.com>
2018-11-29 09:23:47 +0700

    Add AVR Studio project files

---
+++ space-evader.asm
@@ -0,0 +1,26 @@
+.include "m8515def.inc"
+
+;***************************************************************************
+;*
+;* "space-evader"
+;*
+;* This is a simple game written for ATmega8515. You pilot a
+;* space aircraft through a the yonder of space filled with meteorites.
+;* 
+;*
+;* - Aditya Pratama - 1706039490
+;* - Giovan Isa Musthofa - 1706040126
+;* - Lulu Ilmaknun Qurotaini - 1706979341
+;* 
+;*
+;***************************************************************************
+
+
+RESET:                    ;init Stack Pointer
+  ldi temp, low(RAMEND)
+  out SPL, temp
+  ldi temp, high(RAMEND)
+  out SPH, temp
+
+forever:
+  rjmp forever
